4d4abe824e89e393c2184935a58a9f8815341e6f
Guy Germana <gtgermana@gmail.com>
2017-05-21 18:21:02 -0400

    Updated to use aggregate function instead of loop

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
-# This script implements the "Getting and Cleaning Data" course Week 4 project.
+# This script implements the "Getting and Cleaning Data" Course Week 4 project. The STEPs below
+# correspond to the Steps in the course project description.
 
 library(dplyr)
 
@@ -30,12 +31,15 @@ activityLabels <- as.character(activityLabels$V2)
 
 # Create the complete test dataset
 testDataset <- cbind(testCodes, testSubjects, testObs)
+rm(testCodes, testSubjects, testObs)
 
 # Create the complete training dataset
 trainingDataset <- cbind(trainingCodes, trainingSubjects, trainingObs)
+rm(trainingCodes, trainingSubjects, trainingObs)
 
 # STEP 1: Merge the training and test datasets to create one dataset
 fullDataset <- rbind(trainingDataset, testDataset)
+rm(trainingDataset, testDataset)
 
 # STEP 2: Extract only the mean and standard deviation variables for each observation
 # That is, keep only the variables (i.e, columns) of interest - those that have mean or std in their names
@@ -44,6 +48,7 @@ featureNames <- read.table("features.txt")
 #Get the indices for the features of interest
 varIndices <- grep("mean|std", featureNames$V2)
 meanStdDataset <- select(fullDataset, c(1:2, (varIndices + 2)))
+rm(fullDataset)
 
 # STEP 3: Use descriptive activity names to name the activities in the dataset
 meanStdDataset <- mutate(meanStdDataset, activity = activityLabels[activity])
@@ -63,26 +68,10 @@ names(meanStdDataset)[3:(length(featureNames) + 2)] = featureNames
 
 # STEP 5: From the dataset in STEP 4, create a second, independent tidy dataset with the average of each 
 # variable for each activity and each subject
-numSubjects = length(unique(meanStdDataset$subjectID))
-numActivityLabels = length(activityLabels)
-numRows = numSubjects * numActivityLabels
-numVars = length(names(meanStdDataset)) - 2
-tidyDataset <- data.frame(activity = rep(1, numRows), subjectID = rep(1, numRows),
-                   matrix(rep(0, numVars * numRows), numRows))
-names(tidyDataset) <- names(meanStdDataset)
+tidyDataset <- aggregate(meanStdDataset[, 3:ncol(meanStdDataset)], list(activity = meanStdDataset$activity,
+                         subjectID = meanStdDataset$subjectID), mean)
+rm(meanStdDataset)
+rm(activityLabels, featureNames, varIndices)
 
-m = 0
-for (i in 1:numActivityLabels) {
-    for (j in 1:numSubjects) {
-        m = m + 1
-        a1 = filter(meanStdDataset, (activity == activityLabels[i]) & (subjectID == j))
-        tidyDataset[m, 1] = activityLabels[i]
-        tidyDataset[m, 2] = j
-        tidyDataset[m, 3:ncol(a1)] <- colMeans(a1[,3:ncol(a1)])
-    }
-}    
-rm(fullDataset, meanStdDataset, testDataset,trainingDataset, testObs, trainingObs)
-rm(testCodes, testSubjects, trainingCodes, trainingSubjects, a1)
-#rm(activityLabels, featureNames, varIndices)
-
+# Order the dataset by activity, then for each activity, by subjectID
 tidyDataset <- arrange(tidyDataset, activity, subjectID)
